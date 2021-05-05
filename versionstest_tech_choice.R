6f8e9148f05d404626ddf5e21704eeac82df1350
Claudius Graebner <claudius.graebner@posteo.de>
2021-05-05 12:44:36 +0200

    Update versionstest

--- versionstest_tech_choice.R
+++ versionstest_tech_choice.R
@@ -4,37 +4,42 @@
 # melden Sie sich bitte mit einem Screenshot der Fehlermeldung bei Claudius Gräbner
 
 # Ich empfehle Ihnen zudem Ihre R Version zu updaten wenn Sie nicht mindestens
-# Version 3.6.1 verwenden. Dieses Sktipt überprüft auch die aktuelle Version von
+# Version 4.0.1 verwenden. Dieses Sktipt überprüft auch die aktuelle Version von
 # R und gibt Ihnen einen Hinweis wenn Sie updaten sollten.
 
 versionstest <- c(
-  as.double(R.version$major)<3,
-  as.double(substr(R.version$minor, 1, 1))<6,
-  as.double(substr(R.version$minor, 1, 1))<6
+  as.double(R.version$major)<4,
+  as.double(substr(R.version$minor, 3, 3))<1
 )
 
 if (TRUE %in% versionstest){
   print(paste0(
     "ACHTUNG! Ihre R-Version ist veraltet! ",
     "Sie verwenden Version ", R.version$major, ".", R.version$minor, 
-    ", aber Sie sollten mindestens Version 3.6.1 verwenden. ",
+    ", aber Sie sollten mindestens Version 4.0.1 verwenden. ",
     "Installieren Sie zum Update R einfach neu!")
   )
 } else{
   print("Super, Ihre Version von R ist ausreichend aktuell!")
 }
 
-
-install.packages(
-  c('devtools', 'shiny', 'tidyverse', 'magrittr', 'ggpubr', 
-    'lattice', 'survival', 'scales', 'igraph', 'Hmisc', 
-    'wesanderson', 'latex2exp')
-  )
+if (!require(devtools)) install.packages('devtools')
+if (!require(shiny)) install.packages('shiny')
+if (!require(tidyverse)) install.packages('tidyverse')
+if (!require(magrittr)) install.packages('magrittr')
+if (!require(ggpubr)) install.packages('ggpubr')
+if (!require(tidyverse)) install.packages('lattice')
+if (!require(magrittr)) install.packages('survival')
+if (!require(ggpubr)) install.packages('scales')
+if (!require(tidyverse)) install.packages('igraph')
+if (!require(magrittr)) install.packages('Hmisc')
+if (!require(ggpubr)) install.packages('wesanderson')
+if (!require(ggpubr)) install.packages('latex2exp')
 
 ggplot_version <- as.character(packageVersion("ggplot2"))
 
-if ((as.double(substr(ggplot_version, 1, 1)) == 3) &  
-    (as.double(substr(ggplot_version, 3, 3)) >= 4) ) {
+if ((as.double(substr(ggplot_version, 1, 1)) >= 3) &  
+    (as.double(substr(ggplot_version, 3, 3)) >= 3) ) {
   print("ggplot2 ist ausreichend aktuell.")
 } else {
   cat(paste0(
